a1bbe9408e1233daf4d1eb825d5069f27ed04f26
vishnuvardhini2028-app <vishnuvardhini2028@gmail.com>
2026-03-19 21:23:27 +0530

    19/3/26

---
+++ hw.js
@@ -0,0 +1,12 @@
+// Declaring variables
+let name = "Vishnu";
+let age = 20;
+let isStudent = true;
+let favoriteColor = "Green";
+
+
+console.log("Personal Information:");
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
+console.log("Favorite Color: " + favoriteColor);
